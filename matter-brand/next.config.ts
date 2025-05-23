2597a0b790ca81f14f40fcc8572b0692e384d114
Cole <colescarter@gmail.com>
2025-05-23 16:45:08 -0700

    Vercel

--- matter-brand/next.config.ts
+++ matter-brand/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  basePath: '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
